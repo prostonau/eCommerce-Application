be880b81b6f4d69cf3dcb868b97fe8c91a8d749e
Artur Kandaurau <kondaurov.archi@gmail.com>
2023-08-16 17:38:59 +0300

    feat: add raw reg form generator

--- src/components/view/core/components/form/index.ts
+++ src/components/view/core/components/form/index.ts
@@ -17,8 +17,7 @@ class Form extends Component {
     const inputLogin = new InputBox('input', 'form__input', 'email', 'login__input', 'Login', true);
     const inputLoginLabel = new Label('label', 'form__label', 'login__input', '', 'Your e-mail');
 
-    loginField.append(inputLogin.render());
-    loginField.append(inputLoginLabel.render());
+    loginField.append(inputLogin.render(), inputLoginLabel.render());
 
     const passwordField = document.createElement('div');
     passwordField.classList.add('form__field');
@@ -26,20 +25,57 @@ class Form extends Component {
     const inputPassword = new InputBox('input', 'form__input', 'password', 'password__input', 'Password', true);
     const inputPasswordLabel = new Label('label', 'form__label', 'password__input', '', 'Your password');
 
-    passwordField.append(inputPassword.render());
-    passwordField.append(inputPasswordLabel.render());
+    passwordField.append(inputPassword.render(), inputPasswordLabel.render());
 
     const submitBtn = document.createElement('button');
-    submitBtn.classList.add('form__butto');
+    submitBtn.classList.add('form__button');
     submitBtn.id = 'login';
     submitBtn.type = 'submit';
-    submitBtn.innerHTML = 'Login';
+    submitBtn.innerHTML = 'Log in';
 
     form.append(loginField);
     form.append(passwordField);
     form.append(submitBtn);
 
     this.container.append(form);
+  }
+
+  generateRegistrationForm() {
+    const form = document.createElement('form');
+    form.classList.add('reg__form');
+
+    const nameField = document.createElement('div');
+    nameField.classList.add('form__field');
+
+    const lastNameField = document.createElement('div');
+    lastNameField.classList.add('form__field');
+
+    const mailField = document.createElement('div');
+    mailField.classList.add('form__field');
+
+    const birthField = document.createElement('div');
+    birthField.classList.add('form__field');
+
+    const adressField = document.createElement('div');
+    adressField.classList.add('form__field');
+
+    const nameInput = new InputBox('input', 'form__input', 'text', 'name__input', 'First Name', true);
+    const nameLabel = new Label('label', 'form__label', 'name__input', '', 'First Name');
+
+    const lastNameInput = new InputBox('input', 'form__input', 'text', 'last-name__input', 'Last Name', true);
+    const lastNameLabel = new Label('label', 'form__label', 'last-name__input', '', 'Last Name');
+
+    const mailInput = new InputBox('input', 'form__input', 'email', 'mail__input', 'E-mail', true);
+    const mailLabel = new Label('label', 'form__label', 'mail__input', '', 'Last Name');
+
+    const birthDateInput = new InputBox('input', 'form__input', 'text', 'birth-date__input', 'date', true);
+    const birthMonthInput = new InputBox('input', 'form__input', 'text', 'birth-month__input', 'month', true);
+    const birthYearInput = new InputBox('input', 'form__input', 'text', 'birth-year__input', 'month', true);
+
+    nameField.append(nameInput.render(), nameLabel.render());
+    lastNameField.append(lastNameInput.render(), lastNameLabel.render());
+    mailField.append(mailInput.render(), mailLabel.render());
+    birthField.append(birthDateInput.render(), birthMonthInput.render(), birthYearInput.render());
   }
 
   render() {
