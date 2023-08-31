9256e50246387b14de589ce44260a9a51423cf5f
Georg II <trajtv@gmail.com>
2023-08-31 22:50:33 +0200

    feat: add props features

--- src/components/controller/apiCatalog.ts
+++ src/components/controller/apiCatalog.ts
@@ -8,10 +8,14 @@ export class apiCatalog extends AppAPI {
     this.barierToken = 'Basic ' + btoa(`${this.secret}:${this.scope}`);
   }
 
-  //https://api.{region}.commercetools.com/{projectKey}/products
+  // https://api.{region}.commercetools.com/{projectKey}/products
+  // fuzzy=true&text.en-us="jacket"
+  // filter=categories.id:subtree("9531655b-f962-4e88-8386-88063584cfd7")
+  // filter=variants.attributes.color.key:"White","Blue"
+  // ${this.apiUrl}/${this.projectKey}/product-projections/search?limit=10&offset=0&text=${encodeURIComponent(searchQuery)}
   queryProducts(
     BEARER_TOKEN: string,
-    props = 'filter=variants.attributes.color:"black"'
+    props = 'filter=variants.attributes.color.key:"White","Blue"'
   ): Promise<ProductResponse | void> {
     const options = {
       method: 'GET',
