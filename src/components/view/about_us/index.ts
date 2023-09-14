0dcf739af28dda0add5e182987a26fde4b9adc87
Artur Kandaurau <kondaurov.archi@gmail.com>
2023-09-15 01:39:41 +0300

    feat: add demo implementation of about-us team members section without styles and detailed info

--- src/components/view/about_us/index.ts
+++ src/components/view/about_us/index.ts
@@ -1,5 +1,8 @@
 import './style.scss';
 import Page from '../core/templates/page';
+// import artur from './images/aarrttuurr.png';
+// import george from './images/Ge1orge.png';
+// import andrey from './images/prostonau.png';
 
 class AboutUsPage extends Page {
   static TextObject = {
@@ -11,6 +14,9 @@ class AboutUsPage extends Page {
   }
 
   createProjectInfo() {
+    const section = document.createElement('section');
+    section.classList.add('about-us__section');
+
     const header = document.createElement('h2');
     header.classList.add('about-us__header');
     header.textContent = 'E-commerce application';
@@ -26,11 +32,72 @@ class AboutUsPage extends Page {
     par2.textContent = `Users can browse through a vast range of products 📚👗👟, view detailed descriptions, add their favorite items to the basket 🛒,
     and proceed to checkout 💳. It includes features such as user registration and login 📝🔐, product search 🔍, product categorization, and sorting
     to make the shopping experience more streamlined and convenient.`;
+
+    section.append(header, par1, par2);
+    return section;
+  }
+
+  createTeamMemberCard(imgSrc: string, membName: string, membRole: string, gitName: string, membInfo: string) {
+    const container = document.createElement('div');
+    container.classList.add('member__card');
+
+    const img = document.createElement('img');
+    img.classList.add('member__image');
+    img.alt = 'team member photo';
+    img.src = imgSrc;
+
+    const name = document.createElement('h2');
+    name.classList.add('member__name');
+    name.textContent = membName;
+
+    const role = document.createElement('h3');
+    role.classList.add('member__role');
+    role.textContent = membRole;
+
+    const info = document.createElement('p');
+    info.classList.add('member__info');
+    info.textContent = membInfo;
+
+    const github = document.createElement('a');
+    github.classList.add('member__github');
+    github.textContent = gitName;
+    github.href = `https://github.com/${gitName}`;
+
+    container.append(img, name, role, info, github);
+    return container;
   }
 
   render() {
     const title = this.createHeaderTitle(AboutUsPage.TextObject.MainTitle);
-    this.container.append(title);
+
+    const memberSection = document.createElement('section');
+    memberSection.classList.add('about-us__section');
+
+    const georg = this.createTeamMemberCard(
+      './images/Ge1orge.png',
+      'Georgii Khvastunov',
+      'Frontend developer',
+      'Ge1o1Ge',
+      `Georgii is a...`
+    );
+
+    const andrey = this.createTeamMemberCard(
+      './images/prostonau.png',
+      'Andrey Kamenshchikov',
+      'Lead, Frontend developer',
+      'prostonau',
+      `Andrey is a...`
+    );
+
+    const artur = this.createTeamMemberCard(
+      './images/aarrttuurr.png',
+      'Artur Kandaurau',
+      'Frontend developer',
+      'aarrttuurr',
+      `Artur is a...`
+    );
+    memberSection.append(georg, andrey, artur);
+    this.container.append(title, this.createProjectInfo(), memberSection);
     return this.container;
   }
 }
