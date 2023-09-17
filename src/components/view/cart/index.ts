7648b2ffb47341abcbb03e22da5de228e8a05ed5
Georg II <trajtv@gmail.com>
2023-09-17 18:34:01 +0200

    feat: impliment empty cart, recalculate prices 4_13-4_15

--- src/components/view/cart/index.ts
+++ src/components/view/cart/index.ts
@@ -26,11 +26,12 @@ export class CartPage extends Page {
     this.bodyContainer = document.createElement('section');
     this.bodyContainer.classList.add('container__cart');
     this.productList = document.createElement('div');
-    this.productList.classList.add('catalog__list');
+    this.productList.classList.add('cart__list');
     this.cartApi = new APICartNau();
     this.productCartAside = document.createElement('aside');
     this.productCartAside.classList.add('cart__asside');
     this.cardContainer = document.createElement('div');
+    this.cardContainer.classList.add('cart__box');
   }
 
   render() {
@@ -48,70 +49,105 @@ export class CartPage extends Page {
   async generateAddedProducts() {
     this.cardContainer.innerHTML = '';
     let cart = await APICartNau.getCartbyCartId(this.cartId, this.token);
-    cart?.lineItems.forEach(async (item) => {
-      const product = await this.api.queryProducts(APICartNau.getToken() || '', `filter=id: "${item.productId}"`);
-      if (product?.results[0]) {
-        const productCartCard = new ProductCardInCart(product?.results[0], this.cartId, item);
-        const productCartCardEl = productCartCard.render();
+    if (cart && cart?.lineItems.length > 0) {
+      cart?.lineItems.forEach(async (item) => {
+        const product = await this.api.queryProducts(APICartNau.getToken() || '', `filter=id: "${item.productId}"`);
+        if (product?.results[0]) {
+          const productCartCard = new ProductCardInCart(product?.results[0], this.cartId, item);
+          const productCartCardEl = productCartCard.render();
 
-        EventDelegator.addDelegatedListener('click', productCartCard.decButton, async () => {
-          productCartCard.inputQuantity.setValue(
-            (+productCartCard.inputQuantity.getValue() - 1 > 0
-              ? +productCartCard.inputQuantity.getValue() - 1
-              : 1
-            ).toString()
-          );
-          cart = await APICartNau.updateProductQuantityInCart(
-            this.cartId,
-            this.token,
-            item.id,
-            +productCartCard.inputQuantity.getValue()
-          );
-          this.generateAssideBar();
-          console.log('Запрос на обновление корзины');
-        });
+          EventDelegator.addDelegatedListener('click', productCartCard.decButton, async () => {
+            productCartCard.inputQuantity.setValue(
+              (+productCartCard.inputQuantity.getValue() - 1 > 0
+                ? +productCartCard.inputQuantity.getValue() - 1
+                : 1
+              ).toString()
+            );
+            cart = await APICartNau.updateProductQuantityInCart(
+              this.cartId,
+              this.token,
+              item.id,
+              +productCartCard.inputQuantity.getValue()
+            );
+            this.generateAssideBar();
+            this.updatePrice(productCartCard);
+            console.log('Запрос на обновление корзины');
+          });
 
-        EventDelegator.addDelegatedListener('click', productCartCard.incButton, async () => {
-          productCartCard.inputQuantity.setValue((+productCartCard.inputQuantity.getValue() + 1).toString());
-          cart = await APICartNau.updateProductQuantityInCart(
-            this.cartId,
-            this.token,
-            item.id,
-            +productCartCard.inputQuantity.getValue()
-          );
-          this.generateAssideBar();
-          console.log('Запрос на обновление корзины');
-        });
+          EventDelegator.addDelegatedListener('click', productCartCard.incButton, async () => {
+            productCartCard.inputQuantity.setValue((+productCartCard.inputQuantity.getValue() + 1).toString());
+            cart = await APICartNau.updateProductQuantityInCart(
+              this.cartId,
+              this.token,
+              item.id,
+              +productCartCard.inputQuantity.getValue()
+            );
+            this.generateAssideBar();
+            this.updatePrice(productCartCard);
+            console.log('Запрос на обновление корзины');
+          });
 
-        EventDelegator.addDelegatedListener('input', productCartCard.inputQuantity.render(), async () => {
-          cart = await APICartNau.updateProductQuantityInCart(
-            this.cartId,
-            this.token,
-            item.id,
-            +productCartCard.inputQuantity.getValue() > 0 ? +productCartCard.inputQuantity.getValue() : 1
-          );
-          this.generateAssideBar();
-          console.log('Запрос на обновление корзины');
-        });
+          EventDelegator.addDelegatedListener('input', productCartCard.inputQuantity.render(), async () => {
+            cart = await APICartNau.updateProductQuantityInCart(
+              this.cartId,
+              this.token,
+              item.id,
+              +productCartCard.inputQuantity.getValue() > 0 ? +productCartCard.inputQuantity.getValue() : 1
+            );
+            this.generateAssideBar();
+            this.updatePrice(productCartCard);
+            console.log('Запрос на обновление корзины');
+          });
 
-        EventDelegator.addDelegatedListener('click', productCartCard.cardToCart, async () => {
-          cart = await APICartNau.updateProductQuantityInCart(this.cartId, this.token, item.id, 0);
-          this.generateAssideBar();
-          APICartNau.showNotification('Removed');
-          productCartCardEl.id = 'removed';
-          console.log('Запрос на обновление корзины');
-        });
+          EventDelegator.addDelegatedListener('click', productCartCard.cardToCart, async () => {
+            cart = await APICartNau.updateProductQuantityInCart(this.cartId, this.token, item.id, 0);
+            this.generateAssideBar();
+            APICartNau.showNotification('Removed');
+            productCartCardEl.id = 'removed';
+            this.updatePrice(productCartCard);
+            console.log('Запрос на обновление корзины');
+          });
 
-        this.cardContainer.append(productCartCardEl);
-      }
-    });
+          this.cardContainer.append(productCartCardEl);
+        }
+      });
+    } else {
+      this.renderEmptyCart();
+      console.log('корзина пуста');
+    }
   }
 
   async generateAssideBar() {
     this.productCartAside.innerHTML = '';
     const cart = await APICartNau.getCartbyCartId(this.cartId, this.token);
     if (cart) {
-      this.productCartAside.innerHTML = `Total price: ${(cart.totalPrice.centAmount / 100).toString()} USD`;
+      if (cart.totalPrice.centAmount == 0) {
+        this.renderEmptyCart();
+        this.productCartAside.classList.add('asside__hidden');
+      } else {
+        this.productCartAside.classList.remove('asside__hidden');
+        this.productCartAside.innerHTML = `Total price: ${(cart.totalPrice.centAmount / 100).toString()} USD`;
+      }
     }
+  }
+
+  updatePrice(productCartCard: ProductCardInCart) {
+    productCartCard.cardPrice.innerHTML = productCartCard.getPrice('USD');
+    if (productCartCard.getPrice('USD').includes('<span')) {
+      productCartCard.cardPrice.classList.add('card__price--discounted');
+    }
+  }
+
+  renderEmptyCart() {
+    this.cardContainer.innerHTML = '';
+    const cartTitle = document.createElement('h3');
+    cartTitle.classList.add('empty_cart__title');
+    cartTitle.innerHTML = 'Cart is Empty <br> <a href="#catalog" class="cart__catalog-link">Go to Catalog</a>';
+
+    const cartImg = document.createElement('img');
+    cartImg.classList.add('empty_cart__img');
+    cartImg.src = './images/empty.png';
+
+    this.cardContainer.append(cartTitle, cartImg);
   }
 }
